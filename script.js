12623bf10b03c0342ec32216ae3d3b1c5988f1b9
Sicco Smit <sicco.smit@gmail.com>
2020-05-22 16:50:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 //sicco
 let hello = "Hello World!";
-//matthoas 
-document.body.style.backgroundImage = "url('https://bit.ly/2Zr2jCv')"; 
+//matthoas = big imbecile awooo daagt me ni it hé maker aoooooo ik wet a huis wone kot er dan it hé baft van peter van de velde //is pit paraat peter van familie das menne pANGGE waarom hebt gij peter van // familie doodgeschoten? 10 gram boter, 20 egram suiker, 2 rpen chocolade, en  nen bicky boureger wa ri ki ki ki ri ki ki ki i ka ka
+//mamoetmeloak
+//jn nej blast menpe
+//ne zak ol
+//oep
+document.write("<div style='background-color: #bb00fa; border-radius: 200px; '><p>We fudged it!</p></div>"); // <-- matThijs
+
+
+//document.body.style.backgroundImage = "url('https://bit.ly/2Zr2jCv')"; 
 //stief3PO
 let sicco = "I am a crazy gamer"; let str = ' '; str += sicco.charAt(13); str += sicco.charAt(14); str += sicco.charAt(11); document.write("NOT "+str.repeat(20));
 //Taloes taloez 
 
-document.write("<div style='background-color: #bb00fa; border-radius: 200px; font-weight: small;'><p>We fudged it!</p></div>");
-
+document.write("<div style='background-color: #bb00fa; border-radius: 200px;'><p>We fudged it!</p></div>");
+ 
 // no
 alert('¿por qué no?');
 alert("¿porceno los dos?");
-
+WIE
 // S*ck It
 //Neil !!
 document.write('<iframe width="560" height="315" src="https://www.youtube.com/embed/hVdOHMZGwOs" frameborder="0" allow="accelerometer; autoplay;" allowfullscreen></iframe>');
@@ -20,17 +27,17 @@ document.write('<iframe width="560" height="315" src="https://www.youtube.com/em
 prompt('wtf is this'); hello = 'I am sicco and i love beer ';
 
 
-//zatte René
+//zatte René // nerd=null 
  setInterval(function() {return window.alert= null;},200);
 // Ann 
-setTimeout(function(){ document.alert("you're no cuuuuutiee, jensborg!"); }, 500);
-setTimeout(function(){ document.window.close() }, 3000);
+setTimeout(function(){ document.alert("you're so NOT cute, jensborg!, you suck!");});// <- was a mistake
+setTimeout(function(){ document.window.close(); }, 3000);
 
 
-//Tim 
+//Tim !;
 for(i=0;++i<50;console.log(i%5?f||i:f+'buzz'))f=i%3?'':'Fizz';
 
-// Doe Rustig Erick ERICK
+// </3
 class Name {constructor(name){this.name= name;}YourName(){console.log(`${hello}. My name is ${this.name}`)}}const e=new Name('Erick'); e.YourName();
 
 
@@ -39,7 +46,7 @@ alert(hello+" ..... "+sicco);
 
 // 
 // nafets krew deog ssj
-console.log('difficult code');
+console.log('difficult code, not so difficult');
 
 //Iri pliz intensifies
 while(true){let i = 1; alert ("quick maths" + i)}
