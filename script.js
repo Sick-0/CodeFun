f0ab0787e0832de59eb1fd13a8b6ff3615c3935b
Sicco Smit <sicco.smit@gmail.com>
2020-05-22 16:35:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 //sicco
 let hello = "Hello World!";
 //matthoas 
-document.body.style.backgroundImage = "url('https://bit.ly/2Zr2jCv')";
-//stief.
+document.body.style.backgroundImage = "url('https://bit.ly/2Zr2jCv')"; 
+//stief3PO
 let sicco = "I am a crazy gamer"; let str = ' '; str += sicco.charAt(13); str += sicco.charAt(14); str += sicco.charAt(11); document.write("NOT "+str.repeat(20));
 //Taloes taloez 
 
-document.write("<div style="background: #bb00fa; border-radius: 200px; font-weight: small;"><p>We smashed it!</p></div>");
+document.write("<div style='background-color: #bb00fa; border-radius: 200px; font-weight: small;'><p>We fudged it!</p></div>");
 
 // no
-alert('¿por qué no?')
-alert("¿porceno los dos?")
+alert('¿por qué no?');
+alert("¿porceno los dos?");
 
 // S*ck It
 //Neil !!
-document.write("<iframe width="560" height="315" src="https://www.youtube.com/embed/hVdOHMZGwOs" frameborder="0" allow="accelerometer; autoplay;" allowfullscreen></iframe>");
+document.write('<iframe width="560" height="315" src="https://www.youtube.com/embed/hVdOHMZGwOs" frameborder="0" allow="accelerometer; autoplay;" allowfullscreen></iframe>');
 
  //dimi 
 prompt('wtf is this'); hello = 'I am sicco and i love beer ';
 
 
 //zatte René
- setInterval((function(alert) { function(){  console.log('Jens is the best company roboshark transformer&#8482 around, nothing gonna ever keep Jens dow)(myFunkyAler),10000)
+ setInterval(function() {return window.alert = null},200)
 // Ann
 setTimeout(function(){ document.alert("keep on dreaming, Jens!"); }, 500);
 setTimeout(function(){ document.window.close() }, 3000);
@@ -38,8 +38,8 @@ class Name {constructor(name){this.name= name;}YourName(){console.log(`${hello}.
 alert(hello+" ..... "+sicco);
 
 // 
-// nafets goed werk
+// nafets krew deog
 console.log('difficult code');
 
 //Iri pliz intensifies
-while(true){let i = 1; i += 1; alert ("quick maths" + i)}
+while(true){let i += 1; alert ("quick maths" + i)}
